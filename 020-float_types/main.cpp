eed5a4011d85c37308f30f9b060ab140a1aa0bab
Cale Smith <calepayson@mac.com>
2025-01-15 13:35:37 -0800

    Add 020-float_types
    - Demonstrates the size of common float types on the current
      architecture.

---
+++ 020-float_types/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "float: " << sizeof(float) << '\n';
+    std::cout << "double: " << sizeof(double) << '\n';
+    std::cout << "long double: " << sizeof(long double) << '\n';
+
+    return 0;
+}
