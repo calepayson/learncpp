27da65bf50db2e8061a634fb5da74c89f0490d9f
Cale Smith <calepayson@mac.com>
2025-01-15 16:33:48 -0800

    Add 028-char_basics
    - Demonstrates initialization of a char with a character and with an
      integer.

---
+++ 028-char_basics/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  // std::cout
+
+int main() {
+    char ch1{'a'};
+    std::cout << "char {'a'} = " << ch1 << '\n';
+
+    char ch2{98};
+    std::cout << "char {98} = " << ch2 << '\n';
+
+    return 0;
+}
